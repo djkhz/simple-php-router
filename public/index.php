99aedc170710bf7041e2109a06ef646350a71de3
djkhz <djkhz@yahoo.com>
2021-09-10 17:52:14 +0700

    fix hta

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+use Demo\Router;
+// load composer dependencies
+require '../vendor/autoload.php';
+
+// Start the routing
+\Demo\Router::start();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '749f252ffba98955311235cc2befc8ab742a151c',
+        'name' => 'pecee/simple-router-demo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pecee/simple-router' => array(
+            'pretty_version' => '4.3.7.2',
+            'version' => '4.3.7.2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pecee/simple-router',
+            'aliases' => array(),
+            'reference' => '032a2ae7e0e2d876599758f85b61bc965a63ea7c',
+            'dev_requirement' => false,
+        ),
+        'pecee/simple-router-demo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '749f252ffba98955311235cc2befc8ab742a151c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
